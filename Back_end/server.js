3daa608eede8140013a5e5f4495d54be0446ebff
Derrick Dahlitz <ddahlitz@iMac-28.lan>
2022-08-26 14:00:52 -0700

    made

--- Back_end/server.js
+++ Back_end/server.js
@@ -1,1 +1,54 @@
-// testing if the branch is working
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+const Schema = require('./models/Schema.js')
+
+
+
+
+//Create route
+app.post('/dealership', (req, res) => {
+    Schema.create(req.body, (err, newCar) => {
+        res.json(newCar)
+    })
+})
+
+//Index route
+app.get('/dealership', (req, res) => {
+    Schema.find({}, (err, allCars) => {
+    res.json(allCars)
+    })
+})
+
+// delete route
+app.delete('/dealership/:id', (req, res) => {
+    Schema.findByIdAndRemove(req.params.id, (err, removedCar) => {
+        res.json(removedCar)
+    })
+})
+
+
+// edit route
+app.put('/dealership/:id', (req, res) => {
+    Schema.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedCar) => {
+        res.json(updatedCar)
+    })
+})
+
+
+/*==============================================================*/
+/*===========================CONNECTIONS========================*/
+/*==============================================================*/
+mongoose.connect('mongodb://localhost:27017/carDealership')
+mongoose.connection.once('open', () => {
+    console.log('Worshiping the the MON GOD')
+})
+
+app.listen(3000, () => {
+    console.log('There is no PORT 3000')
+})
--- server.js
+++
@@ -1,1 +0,0 @@
-// testing if the branch is working
